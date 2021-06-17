0379bd272222c8d563a95a5e88d3ddb296e14f37
Matchikins <ti4.eas@gmail.com>
2021-06-17 19:26:06 -0300

    Parece que agora vai

---
+++ lib/Src/views/viewsModel/aluno_ViewModel.dart
@@ -0,0 +1,8 @@
+import 'package:cefops/Src/Interface/userInterface.dart';
+import 'package:cefops/Src/models/aluno_Models.dart';
+import 'package:flutter/cupertino.dart';
+
+class userModelView{
+  final IUserRepository
+  final alunomodel= ValueNotifier<alunosModel>(null);
+}
