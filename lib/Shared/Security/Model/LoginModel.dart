83133c9a046964797b2c1a71ba4bf8e5596d7c6c
Emison <84336779+Matchikins@users.noreply.github.com>
2021-11-03 16:59:12 -0300

    implementando login 2.0 erro 000003

--- lib/Shared/Security/Model/LoginModel.dart
+++ lib/Shared/Security/Model/LoginModel.dart
@@ -1,65 +1,65 @@
-class LoginModel {
-  LoginModel({
-    required this.role,
-    required this.fullInfo,
-    required this.token,
-  });
-  late final List<String> role;
-  late final FullInfo fullInfo;
-  late final String token;
-
-  LoginModel.fromJson(Map<String, dynamic> json){
-    role = List.castFrom<dynamic, String>(json['role']);
-    fullInfo = FullInfo.fromJson(json['fullInfo']);
-    token = json['token'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final _data = <String, dynamic>{};
-    _data['role'] = role;
-    _data['fullInfo'] = fullInfo.toJson();
-    _data['token'] = token;
-    return _data;
-  }
-}
-
-class FullInfo {
-  FullInfo({
-    required this.id,
-    required this.name,
-    required this.lastName,
-    required this.cpf,
-    required this.email,
-    required this.photo,
-    required this.enable,
-  });
-  late final String id;
-  late final String name;
-  late final String lastName;
-  late final String cpf;
-  late final String email;
-  late final String photo;
-  late final bool enable;
-
-  FullInfo.fromJson(Map<String, dynamic> json){
-    id = json['id'];
-    name = json['name'];
-    lastName = json['lastName'];
-    cpf = json['cpf'];
-    email = json['email'];
-    photo = json['photo'];
-    enable = json['enable'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final _data = <String, dynamic>{};
-    _data['id'] = id;
-    _data['name'] = name;
-    _data['lastName'] = lastName;
-    _data['cpf'] = cpf;
-    _data['email'] = email;
-    _data['photo'] = photo;
-    _data['enable'] = enable;
-    return _data;
-  }
-}
+// class LoginModel {
+//   LoginModel({
+//     required this.role,
+//     required this.fullInfo,
+//     required this.token,
+//   });
+//   late final List<String> role;
+//   late final FullInfo fullInfo;
+//   late final String token;
+//
+//   LoginModel.fromJson(Map<String, dynamic> json){
+//     role = List.castFrom<dynamic, String>(json['role']);
+//     fullInfo = FullInfo.fromJson(json['fullInfo']);
+//     token = json['token'];
+//   }
+//
+//   Map<String, dynamic> toJson() {
+//     final _data = <String, dynamic>{};
+//     _data['role'] = role;
+//     _data['fullInfo'] = fullInfo.toJson();
+//     _data['token'] = token;
+//     return _data;
+//   }
+// }
+//
+// class FullInfo {
+//   FullInfo({
+//     required this.id,
+//     required this.name,
+//     required this.lastName,
+//     required this.cpf,
+//     required this.email,
+//     required this.photo,
+//     required this.enable,
+//   });
+//   late final String id;
+//   late final String name;
+//   late final String lastName;
+//   late final String cpf;
+//   late final String email;
+//   late final String photo;
+//   late final bool enable;
+//
+//   FullInfo.fromJson(Map<String, dynamic> json){
+//     id = json['id'];
+//     name = json['name'];
+//     lastName = json['lastName'];
+//     cpf = json['cpf'];
+//     email = json['email'];
+//     photo = json['photo'];
+//     enable = json['enable'];
+//   }
+//
+//   Map<String, dynamic> toJson() {
+//     final _data = <String, dynamic>{};
+//     _data['id'] = id;
+//     _data['name'] = name;
+//     _data['lastName'] = lastName;
+//     _data['cpf'] = cpf;
+//     _data['email'] = email;
+//     _data['photo'] = photo;
+//     _data['enable'] = enable;
+//     return _data;
+//   }
+// }
---
+++ lib/Shared/Security/Model/login_model.dart
@@ -0,0 +1,44 @@
+class LoginModel {
+  LoginModel({
+      String? alunoId, 
+      List<String>? role, 
+      String? foto, 
+      String? full Name, 
+      String? token,}){
+    _alunoId = alunoId;
+    _role = role;
+    _foto = foto;
+    _full Name = full Name;
+    _token = token;
+}
+
+  LoginModel.fromJson(dynamic json) {
+    _alunoId = json['AlunoId'];
+    _role = json['role'] != null ? json['role'].cast<String>() : [];
+    _foto = json['foto'];
+    _full Name = json['Full Name'];
+    _token = json['token'];
+  }
+  String? _alunoId;
+  List<String>? _role;
+  String? _foto;
+  String? _full Name;
+  String? _token;
+
+  String? get alunoId => _alunoId;
+  List<String>? get role => _role;
+  String? get foto => _foto;
+  String? get full Name => _full Name;
+  String? get token => _token;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['AlunoId'] = _alunoId;
+    map['role'] = _role;
+    map['foto'] = _foto;
+    map['Full Name'] = _full Name;
+    map['token'] = _token;
+    return map;
+  }
+
+}
